24968e55fb983fd8b51e56d5b2390120d7ac195a
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2021-05-31 18:32:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -122,8 +122,9 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where=''),  # Required
-
+    # packages=find_packages(where=''),  # Required
+    # packages=['pypgtable']
+    
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
