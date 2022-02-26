29a2e3391b18439448535f47dc656cb3e949cf65
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2022-02-26 17:35:34 -0500

    Latest

--- pypgtable/__init__.py
+++ pypgtable/__init__.py
@@ -3,5 +3,6 @@ from obscure_password import obscure
 
 from .raw_table import default_config, raw_table
 from .table import table
+from .database import db_disconnect_all
 
-__all__ = ['table', 'raw_table', 'default_config', 'obscure']
+__all__ = ['table', 'raw_table', 'default_config', 'obscure', 'db_disconnect_all']
